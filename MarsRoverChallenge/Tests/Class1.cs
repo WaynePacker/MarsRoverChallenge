e1087cb1a7a20605f2c6a508e5460e372608321d
Anthony <Wapacker101@gmail.com>
2020-02-01 16:09:22 +0200

    Initial Commit
    Added Project and Test Harness

---
+++ MarsRoverChallenge/Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Foo()
+        {
+            Assert.Pass();
+        }
+    }
+}
